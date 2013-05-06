5dd1bd8f12d5254de0c73ea6e44503561147f241
Lee, Chun-Yi <jlee@suse.com>
2013-05-06 17:09:39 +0800

    asymmetric keys: attempt to decode the PKCS1 private key blob
    Call the pkcs1 private key decoder in pkcs1_private_key.c for attempt to
    decode the PKCS1 private key blob.
    The mpi number allow in private key blob is 8, but we do not grab
    exponent1, exponent2 and coefficient at this moment.
    
    Signed-off-by: Lee, Chun-Yi <jlee@suse.com>

--- crypto/asymmetric_keys/pkcs1_private_key.c
+++ crypto/asymmetric_keys/pkcs1_private_key.c
@@ -11,22 +11,42 @@
 
 #define pr_fmt(fmt) "PKCS1: "fmt
 #include <linux/module.h>
+#include <linux/slab.h>
 #include <keys/asymmetric-subtype.h>
 #include <keys/asymmetric-parser.h>
+#include "public_key.h"
 #include "pkcs1_privatekey-asn1.h"
+
+struct pkcs1_privatekey_parse_context {
+	struct public_key *pub;		/* Public key details */
+	unsigned char nr_mpi;		/* Number of MPIs stored */
+};
 
 /*
  * Attempt to parse a data blob for a private key.
  */
 static int pkcs1_private_key_preparse(struct key_preparsed_payload *prep)
 {
+	struct pkcs1_privatekey_parse_context *ctx;
+	const void *data = prep->data;
+	size_t datalen = prep->datalen;
 	int ret;
 
-	pr_info("pkcs1_private_key.c::pkcs1_private_key_preparse");
+	pr_info("pkcs1_private_key_preparse start\n");
 
-	ret = -EKEYREJECTED;
+	ret = -ENOMEM;
+	ctx = kzalloc(sizeof(struct pkcs1_privatekey_parse_context),
+			GFP_KERNEL);
+	if (!ctx)
+		goto error_no_ctx;
+	ctx->pub = kzalloc(sizeof(struct public_key), GFP_KERNEL);
+	if (!ctx->pub)
+		goto error_no_pub;
 
-	/* TODO: Attempt to decode the PKCS1 private key blob */
+	/* Attempt to decode the PKCS1 private key blob */
+	ret = asn1_ber_decoder(&pkcs1_privatekey_decoder, ctx, data, datalen);
+	if (ret < 0)
+		goto error_decode;
 
 	/* TODO: check the parser result */
 
@@ -36,6 +56,15 @@ static int pkcs1_private_key_preparse(struct key_preparsed_payload *prep)
 
 	/* TODO: We're pinning the module by being linked against it */
 
+	pr_info("pkcs1_private_key_preparse done\n");
+
+	return ret;
+
+error_decode:
+	public_key_destroy(ctx->pub);
+error_no_pub:
+	kfree(ctx);
+error_no_ctx:
 	return ret;
 }
 
@@ -43,6 +72,25 @@ int rsa_privatekey_extract_mpi(void *context, size_t hdrlen,
 				unsigned char tag,
 				const void *value, size_t vlen)
 {
+	struct pkcs1_privatekey_parse_context *ctx = context;
+	MPI mpi;
+
+	if (ctx->nr_mpi >= ARRAY_SIZE(ctx->pub->mpi)) {
+		/* does not grab exponent1, exponent2 and coefficient */
+		if (ctx->nr_mpi > 8) {
+			pr_err("Too many public key MPIs in pkcs1 private key\n");
+			return -EBADMSG;
+		} else {
+			ctx->nr_mpi++;
+			return 0;
+		}
+	}
+
+	mpi = mpi_read_raw_data(value, vlen);
+	if (!mpi)
+		return -ENOMEM;
+
+	ctx->pub->mpi[ctx->nr_mpi++] = mpi;
 	return 0;
 }
 
